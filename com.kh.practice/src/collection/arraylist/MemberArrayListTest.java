f8d56bd126636ead173a9872e98b5aba32474333
김영후 <@172.30.1.73>
2023-08-10 00:57:08 +0900

    d

--- com.kh.practice/src/collection/arraylist/MemberArrayListTest.java
+++ com.kh.practice/src/collection/arraylist/MemberArrayListTest.java
@@ -1,5 +1,8 @@
 package collection.arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import collection.Member;
 
 public class MemberArrayListTest {
@@ -11,12 +14,24 @@ public class MemberArrayListTest {
 		Member memberLee = new Member(101,"이순신");
 		Member memberKim = new Member(101,"김유신");
 		Member memberShin = new Member(101,"신사임당");
-
+		
 		memberArrayList.addMember(memberShin);
 		memberArrayList.addMember(memberLee);
 		memberArrayList.addMember(memberKim);
 
 		memberArrayList.showAll();
+		
+		System.out.println("-----------------------------------------");
+		
+		List<Member> list = new ArrayList<>();
+		
+		list.add(memberShin);
+		list.add(memberLee);
+		list.add(memberKim);
+		
+		for(Member mem : list) {
+			System.out.println(mem);
+		}
 	}
 
 }
--- com.kh.practice/src/generics/Powder.java
+++ com.kh.practice/src/generics/Powder.java
@@ -8,11 +8,7 @@ public class Powder extends Material{
 
 	@Override
 	public void doPrinting() {
-		// TODO Auto-generated method stub
 		System.out.println("Powder 재료로 출력합니다.");
 	}
-	
-
-	
 	
 }
